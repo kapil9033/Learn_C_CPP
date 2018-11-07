48a0bdffb930105cfd6b110e0569f4d50551701a
kapil bhayani <kapilbhayani9033@gmail.com>
2018-11-07 10:58:14 +0100

    Add files via upload

---
+++ Hello.c
@@ -0,0 +1,25 @@
+/* 
+*****This is Comment*********
+-developed at Bell Laboratories in 1972 by Dennis Ritchie.
+-There are 32Keywords in C.
+
+#include <string.h>*/
+
+#include <stdio.h>
+
+	int main(){
+
+		printf("hello World\n");
+		printf("My self Kapil");
+		return 0;
+	}
+
+
+/*int main(){
+
+hello();
+
+return 0;
+}*/
+
+
